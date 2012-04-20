e94a3fcdd243e725ff045d072c3ba7e169b8ba10
roninsoad@gmail.com <roninsoad@gmail.com@584b323f-5f8b-5a45-901e-0a87e86c13b8>
2012-04-20 02:59:18 +0000

    Criação arquitetura.

---
+++ trunk/fontes/manager-tran-school/src/com/br/managertranschool/business/entity/Usuario.java
@@ -0,0 +1,12 @@
+package com.br.managertranschool.business.entity;
+
+
+/**
+ * Classe responsável pela entidade de Usuario.
+ * 
+ * @author Jonatas O. Menezes (menezes.jonatas@hotmail.com)
+ * @since 19/04/2012
+ */
+public class Usuario {
+
+}
--- trunk/fontes/manager-tran-school/src/com/br/managertranschool/view/activity/MainActivity.java
+++ trunk/fontes/manager-tran-school/src/com/br/managertranschool/view/activity/MainActivity.java
@@ -1,8 +1,16 @@
-package com.br.managertranschool;
+package com.br.managertranschool.view.activity;
 
 import android.app.Activity;
 import android.os.Bundle;
 
+import com.br.managertranschool.R;
+
+/**
+ * Classe activity responsavel pela view principal da aplicacao.
+ * 
+ * @author Jonatas O. Menezes (menezes.jonatas@hotmail.com)
+ * @since 19/04/2012
+ */
 public class MainActivity extends Activity {
     /** Called when the activity is first created. */
     @Override
