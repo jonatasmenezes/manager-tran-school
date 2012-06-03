bedd98196a6d9a5c15ad721168447fdab8eec3e6
jef.henrique.07 <jef.henrique.07@584b323f-5f8b-5a45-901e-0a87e86c13b8>
2012-06-03 21:42:07 +0000

    Criação dos DAO e Filter de cliente, localidade e rota

---
+++ fontes/manager-tran-school/src/com/br/managertranschool/business/filter/ClienteFilter.java
@@ -0,0 +1,59 @@
+package com.br.managertranschool.business.filter;
+
+import java.io.Serializable;
+
+import com.br.managertranschool.business.vo.ClienteVO;
+
+
+/**
+ * Classe de filtro de Cliente
+ * 
+ * @author Jeferson Almeida (jef.henrique.07@gmail.com)
+ * @since 03/06/2012
+ */
+public class ClienteFilter implements Serializable{
+
+    private static final long serialVersionUID = -621844318171851775L;
+    
+    private ClienteVO cliente;
+    
+    /**
+     * Construtor padrão.
+     * 
+     * @author Jeferson Almeida (jef.henrique.07@gmail.com)
+     */
+    public ClienteFilter() {
+
+        super();
+    }
+    
+    /**
+     * Construtor com argumento.
+     * 
+     * @param cliente - Objeto {@link ClienteVO}
+     * @author Jeferson Almeida (jef.henrique.07@gmail.com)
+     */
+    public ClienteFilter(ClienteVO cliente) {
+
+        super();
+        this.cliente = cliente;
+    }
+    
+    /**
+     * @return the cliente
+     */
+    public ClienteVO getCliente() {
+
+        return cliente;
+    }
+
+    /**
+     * @param usuario - the usuario to set
+     */
+    public void setCliente(ClienteVO cliente) {
+
+        this.cliente = cliente;
+    }
+
+
+}
---
+++ fontes/manager-tran-school/src/com/br/managertranschool/business/filter/RotaFilter.java
@@ -0,0 +1,57 @@
+package com.br.managertranschool.business.filter;
+
+import java.io.Serializable;
+
+import com.br.managertranschool.business.vo.RotaVO;
+
+
+/**
+ * Classe de filtro de Rota
+ * 
+ * @author Jeferson Almeida (jef.henrique.07@gmail.com)
+ * @since 03/06/2012
+ */
+public class RotaFilter  implements Serializable{
+
+    private static final long serialVersionUID = -4582339798666753057L;
+
+    private RotaVO rota;
+    
+    /**
+     * Construtor padrão.
+     * 
+     * @author Jeferson Almeida (jef.henrique.07@gmail.com)
+     */
+    public RotaFilter() {
+
+        super();
+    }
+
+    /**
+     * Construtor com argumento.
+     * 
+     * @param usuario - Objeto {@link RotaVO}
+     * @author Jeferson Almeida (jef.henrique.07@gmail.com)
+     */
+    public RotaFilter(RotaVO rota) {
+
+        super();
+        this.rota = rota;
+    }
+
+    /**
+     * @return the rota
+     */
+    public RotaVO getRota() {
+
+        return rota;
+    }
+
+    /**
+     * @param rota - the rota to set
+     */
+    public void setRota(RotaVO rota) {
+
+        this.rota = rota;
+    }
+}
