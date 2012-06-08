1858da47b0d3bc28ee7aa1e56fa3017d38a198ce
jef.henrique.07 <jef.henrique.07@584b323f-5f8b-5a45-901e-0a87e86c13b8>
2012-06-08 02:26:09 +0000

    Feito funções basicas dos services de cliente, cidade, localidade, rota e rotaHistorico

---
+++ fontes/manager-tran-school/src/com/br/managertranschool/business/service/CidadeService.java
@@ -0,0 +1,155 @@
+package com.br.managertranschool.business.service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.inject.Inject;
+
+import com.br.managertranschool.R;
+import com.br.managertranschool.architecture.BaseService;
+import com.br.managertranschool.business.filter.CidadeFilter;
+import com.br.managertranschool.business.list.TipoMensagemList;
+import com.br.managertranschool.business.vo.CidadeVO;
+import com.br.managertranschool.dao.CidadeDAO;
+
+/**
+ * Classe de negocio responsavel pela entidade de Cidade.
+ * 
+ * @author Jeferson Almeida (jef.henrique.07@gmail.com)
+ * @since 07/05/2012
+ */
+public class CidadeService extends BaseService {
+    
+    @Inject
+    private CidadeDAO cidadeDAO;
+    
+    /**
+     * Construtor padrão.
+     * 
+     * @author Jeferson Almeida (jef.henrique.07@gmail.com)
+     */
+    public CidadeService() {
+
+        super();
+    }
+
+    /**
+     * Método responsável em realizar a busca de cidade por id.
+     * 
+     * @param cidade - Objeto {@link CidadeVO}.
+     * @return Objeto {@link CidadeVO}.
+     * @author Jeferson Almeida (jef.henrique.07@gmail.com)
+     */
+    public CidadeVO buscarPorId(CidadeVO cidade) {
+
+        this.validarIdObrigatorio(cidade);
+        CidadeVO cidadeReturn = null;
+        
+        if (super.isValido()) {
+            cidadeReturn = cidadeDAO.buscarPorId(cidade.getId()); 
+        }
+        
+        return cidadeReturn;
+    }
+
+    /**
+     * Método responsável em obter lista de usuários.
+     * 
+     * @param filter - Filtro de Cidade {@link CidadeFilter}.
+     * @return - Lista de usuários.
+     * @author Jeferson Almeida (jef.henrique.07@gmail.com)
+     */
+    public List<CidadeVO> pesquisar(CidadeFilter filter) {
+
+        List<CidadeVO> cidadeList = new ArrayList<CidadeVO>();
+        
+        cidadeList = cidadeDAO.pesquisar(filter);
+        if (cidadeList.isEmpty()) {
+            super.addMensagem(R.string.pesquisa_nao_encontrou_resultados, TipoMensagemList.INFORMACAO);
+        }
+        
+        return cidadeList;
+    }
+
+    /**
+     * Método responsável em inserir um novo usuário.
+     * 
+     * @param cidade - Objeto {@link CidadeVO}.
+     * @author Jeferson Almeida (jef.henrique.07@gmail.com)
+     */
+    public void salvar(CidadeVO cidade) {
+
+        this.validarCamposObrigatorios(cidade);
+        
+        if (super.isValido()) {
+            cidadeDAO.salvar(cidade); 
+            super.addMensagem(R.string.inclusao_sucesso, TipoMensagemList.INFORMACAO);
+        }
+    }
+
+    /**
+     * Método responsável em atualizar dados do usuário.
+     * 
+     * @param cidade - Objeto {@link CidadeVO}.
+     * @author Jeferson Almeida (jef.henrique.07@gmail.com)
+     * @throws Exception 
+     */
+    public void atualizar(CidadeVO cidade) throws Exception {
+
+        this.validarIdObrigatorio(cidade);
+        this.validarCamposObrigatorios(cidade);
+        
+        if (super.isValido()) {
+            cidadeDAO.atualizar(cidade); 
+            super.addMensagem(R.string.alteracao_sucesso, TipoMensagemList.INFORMACAO);
+        }
+    }
+
+    /**
+     * Método responsável em excluir dados do cidade.
+     * 
+     * @param cidade - Objeto {@link CidadeVO}.
+     * @author Jeferson Almeida (jef.henrique.07@gmail.com)
+     * @throws Exception 
+     */
+    public void delete(CidadeVO cidade) throws Exception {
+
+        this.validarIdObrigatorio(cidade);
+        
+        if (super.isValido()) {
+            cidadeDAO.delete(cidade.getId()); 
+            super.addMensagem(R.string.exclusao_sucesso, TipoMensagemList.INFORMACAO);
+        }
+    }
+
+      
+    /**
+     * Método valida se id foi informado.
+     * 
+     * @param cidade - Objeto CidadeVO
+     * @author Jeferson Almeida (jef.henrique.07@gmail.com)
+     */
+    private void validarIdObrigatorio(CidadeVO cidade) {
+        if (!super.isNotNullAndNotEmpty(cidade.getId())) {
+            super.addMensagem(R.string.cidade_id_obrigatorio, TipoMensagemList.ERRO);
+        }
+    }
+    
+    /**
+     * Método valida se campos obrigatorios foram informados.
+     * 
+     * @param cidade - Objeto CidadeVO
+     * @author Jeferson Almeida (jef.henrique.07@gmail.com)
+     */
+    private void validarCamposObrigatorios(CidadeVO cidade) {
+        if (!super.isNotNullAndNotEmpty(cidade.getEstadoId())) {
+            super.addMensagem(R.string.cidade_estado_id_obrigatorio, TipoMensagemList.ERRO);
+        }
+        
+        if (!super.isNotNullAndNotEmpty(cidade.getDescricao())) {
+            super.addMensagem(R.string.cidade_descricao_obrigatorio, TipoMensagemList.ERRO);
+        }
+
+    }
+    
+}
---
+++ fontes/manager-tran-school/src/com/br/managertranschool/business/service/RotaHistoricoService.java
@@ -0,0 +1,162 @@
+package com.br.managertranschool.business.service;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import javax.inject.Inject;
+
+import com.br.managertranschool.R;
+import com.br.managertranschool.architecture.BaseService;
+import com.br.managertranschool.business.filter.RotaHistoricoFilter;
+import com.br.managertranschool.business.list.TipoMensagemList;
+import com.br.managertranschool.business.vo.RotaHistoricoVO;
+import com.br.managertranschool.dao.RotaHistoricoDAO;
+
+/**
+ * Classe de negocio responsavel pela entidade de RotaHistorico.
+ * 
+ * @author Jeferson Almeida (jef.henrique.07@gmail.com)
+ * @since 07/05/2012
+ */
+public class RotaHistoricoService extends BaseService {
+    
+    @Inject
+    private RotaHistoricoDAO rotaHistoricoDAO;
+    
+    /**
+     * Construtor padrão.
+     * 
+     * @author Jeferson Almeida (jef.henrique.07@gmail.com)
+     */
+    public RotaHistoricoService() {
+
+        super();
+    }
+
+    /**
+     * Método responsável em realizar a busca de rotaHistorico por id.
+     * 
+     * @param rotaHistorico - Objeto {@link RotaHistoricoVO}.
+     * @return Objeto {@link RotaHistoricoVO}.
+     * @author Jeferson Almeida (jef.henrique.07@gmail.com)
+     */
+    public RotaHistoricoVO buscarPorId(RotaHistoricoVO rotaHistorico) {
+
+        this.validarIdObrigatorio(rotaHistorico);
+        RotaHistoricoVO rotaHistoricoReturn = null;
+        
+        if (super.isValido()) {
+            rotaHistoricoReturn = rotaHistoricoDAO.buscarPorId(rotaHistorico.getId()); 
+        }
+        
+        return rotaHistoricoReturn;
+    }
+
+    /**
+     * Método responsável em obter lista de usuários.
+     * 
+     * @param filter - Filtro de RotaHistorico {@link RotaHistoricoFilter}.
+     * @return - Lista de usuários.
+     * @author Jeferson Almeida (jef.henrique.07@gmail.com)
+     */
+    public List<RotaHistoricoVO> pesquisar(RotaHistoricoFilter filter) {
+
+        List<RotaHistoricoVO> rotaHistoricoList = new ArrayList<RotaHistoricoVO>();
+        
+        rotaHistoricoList = rotaHistoricoDAO.pesquisar(filter);
+        if (rotaHistoricoList.isEmpty()) {
+            super.addMensagem(R.string.pesquisa_nao_encontrou_resultados, TipoMensagemList.INFORMACAO);
+        }
+        
+        return rotaHistoricoList;
+    }
+
+    /**
+     * Método responsável em inserir um novo usuário.
+     * 
+     * @param rotaHistorico - Objeto {@link RotaHistoricoVO}.
+     * @author Jeferson Almeida (jef.henrique.07@gmail.com)
+     */
+    public void salvar(RotaHistoricoVO rotaHistorico) {
+
+        this.validarCamposObrigatorios(rotaHistorico);
+        
+        if (super.isValido()) {
+            rotaHistoricoDAO.salvar(rotaHistorico); 
+            super.addMensagem(R.string.inclusao_sucesso, TipoMensagemList.INFORMACAO);
+        }
+    }
+
+    /**
+     * Método responsável em atualizar dados do usuário.
+     * 
+     * @param rotaHistorico - Objeto {@link RotaHistoricoVO}.
+     * @author Jeferson Almeida (jef.henrique.07@gmail.com)
+     * @throws Exception 
+     */
+    public void atualizar(RotaHistoricoVO rotaHistorico) throws Exception {
+        
+        rotaHistorico.setData(new Date());
+        
+        this.validarIdObrigatorio(rotaHistorico);
+        this.validarCamposObrigatorios(rotaHistorico);
+        
+        if (super.isValido()) {            
+            rotaHistoricoDAO.atualizar(rotaHistorico); 
+            super.addMensagem(R.string.alteracao_sucesso, TipoMensagemList.INFORMACAO);
+        }
+    }
+
+    /**
+     * Método responsável em excluir dados do rotaHistorico.
+     * 
+     * @param rotaHistorico - Objeto {@link RotaHistoricoVO}.
+     * @author Jeferson Almeida (jef.henrique.07@gmail.com)
+     * @throws Exception 
+     */
+    public void delete(RotaHistoricoVO rotaHistorico) throws Exception {
+
+        this.validarIdObrigatorio(rotaHistorico);
+        
+        if (super.isValido()) {
+            rotaHistoricoDAO.delete(rotaHistorico.getId()); 
+            super.addMensagem(R.string.exclusao_sucesso, TipoMensagemList.INFORMACAO);
+        }
+    }
+
+      
+    /**
+     * Método valida se id foi informado.
+     * 
+     * @param rotaHistorico - Objeto RotaHistoricoVO
+     * @author Jeferson Almeida (jef.henrique.07@gmail.com)
+     */
+    private void validarIdObrigatorio(RotaHistoricoVO rotaHistorico) {
+        if (!super.isNotNullAndNotEmpty(rotaHistorico.getId())) {
+            super.addMensagem(R.string.rota_historico_id_obrigatorio, TipoMensagemList.ERRO);
+        }
+    }
+    
+    /**
+     * Método valida se campos obrigatorios foram informados.
+     * 
+     * @param rotaHistorico - Objeto RotaHistoricoVO
+     * @author Jeferson Almeida (jef.henrique.07@gmail.com)
+     */
+    private void validarCamposObrigatorios(RotaHistoricoVO rotaHistorico) {
+        if (!super.isNotNullAndNotEmpty(rotaHistorico.getRotaId())) {
+            super.addMensagem(R.string.rota_historico_rota_id_obrigatorio, TipoMensagemList.ERRO);
+        }
+        
+        if (!super.isNotNullAndNotEmpty(rotaHistorico.getData())) {
+            super.addMensagem(R.string.rota_historico_data_obrigatorio, TipoMensagemList.ERRO);
+        }        
+        
+        if (!super.isNotNullAndNotEmpty(rotaHistorico.getUsuarioId())) {
+            super.addMensagem(R.string.rota_historico_usuario_id_obrigatorio, TipoMensagemList.ERRO);
+        }
+
+    }
+    
+}
